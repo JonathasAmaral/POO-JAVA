41719239434ffaa2a03a94087f601ced05adf42e
Jonathas <jonathasamaral5@gmail.com>
2020-09-16 01:35:53 -0300

    Inclusão de Exemplo POO

---
+++ src/advanced/MainPeople.java
@@ -0,0 +1,20 @@
+package advanced;
+
+public class MainPeople {
+	public static void main(String[] args) {
+		
+		People p1 = new People();
+		p1.nome = "Jonathas";
+		p1.cpf = "274.564.476-54";
+		p1.idade = 19;
+		p1.peso = 53.5;
+		p1.teste();
+		People p2 = new People();
+		p2.nome = "Pedro";
+		p2.cpf = "254.435.234-43";
+		p2.idade = 12;
+		p2.peso = 33.5;
+        p2.teste();
+	}
+
+}
