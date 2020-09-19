b8277ccb871c7293279515a681f4c41fead4159c
Jonathas Amaral <69598952+JonathasAmaral@users.noreply.github.com>
2020-09-18 23:03:24 -0300

    Update MainPeople.java

--- src/advanced/MainPeople.java
+++ src/advanced/MainPeople.java
@@ -14,13 +14,8 @@ public class MainPeople {
 		p2.cpf = "254.435.234-43";
 		p2.idade = 12;
 		p2.peso = 33.5;
-        p2.teste();
-        People p3 = new People();
-		p3.nome = "Julio";
-		p3.cpf = "435.824.253-68";
-		p3.idade = 22;
-		p3.peso = 102.00;
-        p3.teste();
+                p2.teste();
+      
 	}
 
 }
